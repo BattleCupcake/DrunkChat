cd933888ac43b8cb49ab3a2176a86c93a09730ca
max <maxkobalt86@gmail.com>
2019-02-19 19:25:13 +0400

    channels

--- app/src/main/java/com/example/drunkchat/Controller/MainActivity.kt
+++ app/src/main/java/com/example/drunkchat/Controller/MainActivity.kt
@@ -14,12 +14,15 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import com.example.drunkchat.Model.Channel
 import com.example.drunkchat.R
 import com.example.drunkchat.Services.AuthService
+import com.example.drunkchat.Services.MessageService
 import com.example.drunkchat.Services.UserDataServices
 import com.example.drunkchat.Utilities.BROADCAST_USER_DATA_CHANGE
 import com.example.drunkchat.Utilities.SOCKET_URL
 import io.socket.client.IO
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
@@ -32,6 +35,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        socket.connect()
+        socket.on("ChannelCreated", onNewChannel)
 
 
         val toggle = ActionBarDrawerToggle(
@@ -44,18 +49,17 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReseiver,
             IntentFilter(BROADCAST_USER_DATA_CHANGE))
-        socket.connect()
+
         super.onResume()
     }
 
 
-    override fun onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangeReseiver)
-        super.onPause()
-    }
+
 
     override fun onDestroy() {
         socket.disconnect()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangeReseiver)
+
         super.onDestroy()
     }
 
@@ -115,6 +119,17 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 .show()
+        }
+    }
+
+    private val onNewChannel = Emitter.Listener { args ->
+        runOnUiThread {
+            val channelName = args[0] as String
+            val channelDescription = args[1] as String
+            val channelId = args[2] as String
+
+            val newChannel = Channel(channelName, channelDescription, channelId)
+            MessageService.channels.add(newChannel)
         }
     }
 
---
+++ app/src/main/java/com/example/drunkchat/Services/MessageService.kt
@@ -0,0 +1,8 @@
+package com.example.drunkchat.Services
+
+import com.example.drunkchat.Model.Channel
+import java.util.ArrayList
+
+object MessageService {
+    val channels  = ArrayList<Channel>()
+}
